9a642ca5700960dc80135e9e731697f68be43221
tal <ykvnkl2@gmail.com>
2022-12-03 12:41:43 +0200

    bag singleton

--- src/test/Bag.java
+++ src/test/Bag.java
@@ -4,7 +4,8 @@ import java.util.Random;
 public class Bag {
     private int[] letter_count;
     private Tile[] tiles;
-    public Bag() {
+    private static Bag bag_instance=null;
+    private Bag() {
         this.letter_count = new int[]{9,2,2,4,12,2,3,2,9,1,1,4,2,6,8,2,1,6,4,6,4,2,2,1,2,1};
         this.tiles=new Tile[26];
         int [] scores = new int[]{1,3,3,2,1,4,2,4,1,8,5,1,3,1,1,3,10,1,1,1,1,4,4,8,4,10};
@@ -12,7 +13,12 @@ public class Bag {
             this.tiles[i] = new Tile((char)('A'+i),scores[i]);
         }
     }
+    public static Bag getBag(){
+       if(bag_instance==null)
+           bag_instance = new Bag();
+       return bag_instance;
 
+    }
     public int[] getLetter_count() {
         return letter_count;
     }
@@ -66,6 +72,21 @@ public class Bag {
         {
             this.letter_count[(int)tile.letter] +=1;
         }
+    }
+    public int size(){
+        int size =0;
+        for (int i:this.letter_count) {
+           size+=i;
+        }
+        return size;
+    }
+    public int[] getQuantities(){
+        int[] temp =  new int[26];
+        for(int i=0;i<26;i++)
+        {
+           temp[i] = this.letter_count[i];
+        }
+        return temp;
     }
 
 }
