40bf364aa0af07d06e374fcff232f376be9d474d
tal <ykvnkl2@gmail.com>
2022-12-04 20:15:03 +0200

    board start

--- src/test/Board.java
+++ src/test/Board.java
@@ -2,5 +2,26 @@ package test;
 
 
 public class Board {
+    private static Board board_instance;
+    private Tile[][] board;
+
+    public static Board getBoard(){
+        if(board_instance==null)
+            board_instance = new Board();
+        return board_instance;
+
+    }
+    public Tile[][] getTiles(){
+        Tile[][] clone = new Tile[15][15];
+        for(int i =0;i<15;i++){
+            for (int j =0;j<15;j++) {
+                clone = this.board[i];
+            }
+        }
+        return clone;
+    }
+    public boolean boardLegal(Word word){
+        
+    }
 
 }
--- src/test/Word.java
+++ src/test/Word.java
@@ -2,6 +2,47 @@ package test;
 
 
 public class Word {
+    private Tile[] tiles;
+    private int row;
+    private int col;
+    private boolean vertical;
 
-	
+    public Word(Tile[] tiles, int row, int col, boolean vertical) {
+        this.tiles = tiles;
+        this.row = row;
+        this.col = col;
+        this.vertical = vertical;
+    }
+    public Tile[] getTiles() {
+        return tiles;
+    }
+
+    public void setTiles(Tile[] tiles) {
+        this.tiles = tiles;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    public void setCol(int col) {
+        this.col = col;
+    }
+
+    public boolean isVertical() {
+        return vertical;
+    }
+
+    public void setVertical(boolean vertical) {
+        this.vertical = vertical;
+    }
 }
