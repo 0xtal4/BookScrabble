d0f613257bf8e6964cd2a52287d99f72b785a854
tal <ykvnkl2@gmail.com>
2022-12-29 16:32:34 +0200

    mile stone 2 done correctly!

--- src/mileStone2/CacheManager.java
+++ src/mileStone2/CacheManager.java
@@ -1,12 +1,25 @@
 package mileStone2;
 
 
-public class CacheManager {
+import java.util.HashSet;
 
-    public interface CacheReplacementPolicy{
-        void add(String word);
-        String remove();
+public class CacheManager {
+    private int size;
+    private HashSet<String> cache;
+    private CacheReplacementPolicy crp;
+    public CacheManager(int size,CacheReplacementPolicy crp){
+        this.size=size;
+        this.crp=crp;
+        cache = new HashSet<String>();
     }
-	
+    public boolean query(String word){return cache.contains(word);}
+    public void add(String word){
+        if (cache.size()>=size){
+            String wordToRemove=crp.remove();
+            cache.remove(wordToRemove);
+        }
+        crp.add(word);
+        cache.add(word);
+    }
 
 }
--- src/mileStone2/Dictionary.java
+++ src/mileStone2/Dictionary.java
@@ -1,6 +1,74 @@
 package mileStone2;
 
 
+import java.io.*;
+
 public class Dictionary {
+    private CacheManager cacheExists;
+    private CacheManager cacheNotExists;
+    private BloomFilter bloom;
+    private String[] fileNames;
+
+    public Dictionary(String... fileNames) {
+        this.fileNames=fileNames;
+        bloom = new BloomFilter(256, "SHA1", "MD5");
+        CacheReplacementPolicy crpLRU = new LRU();
+        CacheReplacementPolicy crpLFU = new LFU();
+        cacheExists = new CacheManager(400, crpLRU);
+        cacheNotExists = new CacheManager(100, crpLFU);
+
+        String line;
+        for(String file:fileNames){
+            BufferedReader reader = null;
+            try {
+                reader = new BufferedReader(new FileReader(file));
+            } catch (FileNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+            while(true){
+                try {
+                    if (!((line = reader.readLine()) != null)) break;
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+                String[] words = line.split("\\s");
+                for (String fileWord: words){
+                    cacheExists.add(fileWord);
+                    bloom.add(fileWord);
+                }
+            }
+            try {
+                reader.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+    }
+
+    public boolean query(String word) {
+        if (cacheExists.query(word))
+            return true;
+        else if (cacheNotExists.query(word))
+            return false;
+        else {
+            if (bloom.contains(word)) {
+                cacheExists.add(word);
+                return true;
+            } else {
+                cacheNotExists.add(word);
+                return false;
+            }
+        }
+    }
+    public boolean challenge(String word){
+        boolean flag;
+        try {
+            return IOSearcher.search(word,fileNames);
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
 
 }
