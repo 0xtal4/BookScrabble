50b6fa198b66009575b5db0edfd24ecbca1a122a
0xtal4 <ykvnkl2@gmail.com>
2023-05-27 23:13:41 +0300

    guest implemented

--- src/main/java/com/bookscrabble/client/model/Guest.java
+++ src/main/java/com/bookscrabble/client/model/Guest.java
@@ -2,67 +2,107 @@ package com.bookscrabble.client.model;
 
 import java.io.*;
 import java.net.Socket;
-import java.util.Scanner;
 
+/**
+ * The Guest class represents a guest player in the Book Scrabble game.
+ * It handles the communication with the game server and provides methods
+ * for connecting, placing words, and performing challenges.
+ */
 public class Guest {
     private static Socket serverConnection;
     private static Writer writer;
     private static BufferedReader reader;
 
-    public int getPoints() {
-        return points;
-    }
-
-    public void setPoints(int points) {
-        this.points = points;
-    }
-
     private int points;
 
+    /**
+     * Constructs a new Guest object.
+     */
     public Guest() {
-        serverConnection = new Socket();
-        points=0;
+        serverConnection = null;
+        points = 0;
         writer = null;
         reader = null;
     }
 
+    /**
+     * Connects to the game server at the specified host and port.
+     *
+     * @param host the host name or IP address of the server
+     * @param port the port number of the server
+     * @throws Exception if there was an error when trying to connect to the server
+     */
     public void connect(String host, int port) throws Exception {
         serverConnection = new Socket(host, port);
         writer = new OutputStreamWriter(serverConnection.getOutputStream());
         reader = new BufferedReader(new InputStreamReader(serverConnection.getInputStream()));
     }
 
+    /**
+     * Tries to place a word on the game board.
+     *
+     * @param word the word to be placed
+     * @throws Exception if there was an error in the connection, the word is invalid,
+     *                   or the dictionary check fails
+     */
     public void tryPlaceWord(Word word) throws Exception {
         if (serverConnection.isConnected()) {
-            writer.write("W,"+word.toString());
+            writer.write("W," + word.toString());
             writer.flush();
 
             String result = reader.readLine();
-            if(result.equals("boardIllegal"))
+            if (result.equals("boardIllegal"))
                 throw new Exception("boardIllegal");
-            if(result.equals("dictionaryIllegal"))
+            if (result.equals("dictionaryIllegal"))
                 throw new Exception("dictionaryIllegal");
-            if(result.equals("error"))
+            if (result.equals("error"))
                 throw new Exception("connectionError");
-            if(result.contains("success")){
+            if (result.contains("success")) {
                 String[] s = result.split(",");
                 int points = Integer.parseInt(s[1]);
-                this.points+=points;
+                this.points += points;
             }
-        } else
+        } else {
             throw new Exception("connectionError");
+        }
     }
+
+    /**
+     * Performs a challenge against a word on the game board.
+     *
+     * @param word the word to challenge
+     * @return true if the challenge is successful, false otherwise
+     * @throws Exception if there was an error in the connection
+     */
     public boolean challenge(Word word) throws Exception {
         if (serverConnection.isConnected()) {
-            writer.write("C,"+word.toString());
+            writer.write("C," + word.toString());
             writer.flush();
             String result = reader.readLine();
-            if(result.equals("true"))
+            if (result.equals("true"))
                 return true;
             else
                 return false;
-
-        } else
+        } else {
             throw new Exception("connectionError");
+        }
+    }
+
+    /**
+     * Gets the current points of the guest player.
+     *
+     * @return the points of the guest player
+     */
+    public int getPoints() {
+        return points;
+    }
+
+    /**
+     * Sets the points of the guest player.
+     *
+     * @param points the points to set
+     */
+    public void setPoints(int points) {
+        this.points = points;
     }
 }
--- src/test/java/com/bookscrabble/client/model/GuestTest.java
+++ src/test/java/com/bookscrabble/client/model/GuestTest.java
@@ -1,8 +1,41 @@
 package com.bookscrabble.client.model;
+
+import com.bookscrabble.server.MainTrain;
+import com.bookscrabble.server.MyServer;
+
 public class GuestTest {
     public static void main(String[] args) {
+        try {
         Guest g =new Guest();
-        Host h = Host.getHost();
+        String[] dictionaries = {"Frank Herbert - Dune.txt", "mobydick.txt"};
+        //create a host
+        Host myHost = Host.getHost();
+        Tile t1 = new Tile('c',9);
+        Tile t2 = new Tile('h',3);
+        Tile[] tiles = new Tile[2];
+        tiles[0] = t1;
+        tiles[1] = t2;
+        myHost.startGame(dictionaries);
+        System.out.println("myhost started");
+        g.connect("localhost",3000);
+        Word w1 = new Word(tiles,-1,0,false);
+        Word w2 = new Word(tiles,1,0,false);
+        try {
+            g.tryPlaceWord(w1);
+            System.out.println("test 1 failed");
+        }catch(Exception e){
+            }
+        try {
+            g.tryPlaceWord(w2);
+            System.out.println("test 2 failed");
+        }catch (Exception e){}
+        System.out.println("done");
 
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            System.out.println("failed to start the server");
+        }
     }
 }
