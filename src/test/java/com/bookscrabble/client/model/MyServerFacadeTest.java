7608dbffa6a3ea059abafd17a02c12c79a5b073b
GalSamy <59654405+GalSamy@users.noreply.github.com>
2023-05-27 19:27:06 +0300

    fixed facade tests

--- src/test/java/com/bookscrabble/client/model/MyServerFacadeTest.java
+++ src/test/java/com/bookscrabble/client/model/MyServerFacadeTest.java
@@ -7,25 +7,25 @@ public class MyServerFacadeTest {
     public static void main(String[] args) throws IOException {
         MyServerFacade f = new MyServerFacade(9999, "localhost");
         if (f.Challenge(new String[]{"test1"}, "word1")){
-            System.out.println("test 1 passed");
+            System.out.println("Challenge test 1 passed");
         }else {
-            System.out.println("test 1 failed");
+            System.out.println("Challenge test 1 failed");
         }
         if (f.Challenge(new String[]{"test2"}, "word2")){
-            System.out.println("test 2 passed");
+            System.out.println("Challenge test 2 passed");
         }else{
-            System.out.println("test 2 failed");
+            System.out.println("Challenge test 2 failed");
 
         }
         if (f.Query(new String[]{"test1"}, "word1")){
-            System.out.println("test 1 passed");
+            System.out.println("Query test 1 passed");
         }else {
-            System.out.println("test 1 failed");
+            System.out.println("Query test 1 failed");
         }
         if (f.Query(new String[]{"test2"}, "word2")){
-            System.out.println("test 2 passed");
+            System.out.println("Query test 2 passed");
         }else{
-            System.out.println("test 2 failed");
+            System.out.println("Query test 2 failed");
 
         }
     }
