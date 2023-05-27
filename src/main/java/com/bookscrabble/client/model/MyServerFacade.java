266f779acd151789ef68d6a54556266d8b65cdeb
Dotan Mendelbaum Laptop <dotan@cogniteam.com>
2023-05-27 20:31:03 +0300

    complete hst functionality

--- src/main/java/com/bookscrabble/client/model/MyServerFacade.java
+++ src/main/java/com/bookscrabble/client/model/MyServerFacade.java
@@ -7,29 +7,29 @@ import java.util.Scanner;
 
 /**
  * this class is made to make communication between the main server and the host easier
- * @param server is our socket to the main server
  */
 public class MyServerFacade {
+
+    //socket to the main server
     Socket server;
     /**
      * this is the builder for the class,it creates the connection to the main server.
-     *
      *
      * @param port the port of the server.
      * @param address the adress of the server. if passed empty set to localhost
      */
     public MyServerFacade(int port, String address) throws IOException {
         if (address == null){
-            address = "localhost"
+            address = "localhost";
         }
-        server = new Socket("localhost", port);
+        server = new Socket(address, port);
     }
+
     /**
      * the challenge function is used to check if given word is in the given books.
      *
-     *
      * @param dictionaries the books that we want to search the word in.
-     * @param query the the word that we want to search
+     * @param query the word that we want to search
      */
     public Boolean Challenge(String[] dictionaries, String query){
         String res;
@@ -46,26 +46,20 @@ public class MyServerFacade {
         }
         return false;
     }
+
     /**
      * the query function is used to check if given word is in the Bloom filter.
-     *
      *
      * @param dictionaries the books that we want to search the word in.
      * @param query the the word that we want to search
      */
-    public Boolean Query(String[] dictionaries, String query){
-        try{
-            PrintWriter out=new PrintWriter(server.getOutputStream());
-            Scanner in=new Scanner(server.getInputStream());
-            String dics = String.join(",", dictionaries);
-            out.println("q,"+dics+"," + query);
-            out.flush();
-            String res=in.next();
-            return Boolean.getBoolean(res);
-        }catch (IOException e) {
-            System.out.println("your code ran into an exception");
-        }
-        return false;
-
+    public Boolean Query(String[] dictionaries, String query) throws IOException{
+        PrintWriter out=new PrintWriter(server.getOutputStream());
+        Scanner in=new Scanner(server.getInputStream());
+        String dics = String.join(",", dictionaries);
+        out.println("q,"+dics+"," + query);
+        out.flush();
+        String res=in.next();
+        return Boolean.getBoolean(res);
     }
 }
--- src/main/java/com/bookscrabble/client/model/main.java
+++
@@ -1,7 +0,0 @@
-package com.bookscrabble.client.model;
-
-public class main {
-    public static void main(String[]args){
-        MyServerFacade m = new MyServce
-    }
-}
