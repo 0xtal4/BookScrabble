064f7102bed1f3abbd76fa4d3b8947a109ea1c09
0xtal4 <ykvnkl2@gmail.com>
2023-05-27 22:41:29 +0300

    guest implemented

--- src/main/java/com/bookscrabble/client/model/Guest.java
+++ src/main/java/com/bookscrabble/client/model/Guest.java
@@ -1,27 +1,68 @@
 package com.bookscrabble.client.model;
 
-import java.io.Console;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Guest {
-
     private static Socket serverConnection;
-    private static OutputStream out;
+    private static Writer writer;
+    private static BufferedReader reader;
 
-    public static void connect(String host, int port) throws Exception{
-        serverConnection = new Socket(host, port);
-        out = serverConnection.getOutputStream();
+    public int getPoints() {
+        return points;
     }
 
-    public static void tryPlaceWord(Word word)
-    {
-        if(serverConnection.isConnected())
-        {
+    public void setPoints(int points) {
+        this.points = points;
+    }
 
-        }
-        else{
-            System.out.println("the connectin with the host was lost....");
-        }
+    private int points;
+
+    public Guest() {
+        serverConnection = new Socket();
+        points=0;
+        writer = null;
+        reader = null;
+    }
+
+    public void connect(String host, int port) throws Exception {
+        serverConnection = new Socket(host, port);
+        writer = new OutputStreamWriter(serverConnection.getOutputStream());
+        reader = new BufferedReader(new InputStreamReader(serverConnection.getInputStream()));
+    }
+
+    public void tryPlaceWord(Word word) throws Exception {
+        if (serverConnection.isConnected()) {
+            writer.write("W,"+word.toString());
+            writer.flush();
+
+            String result = reader.readLine();
+            if(result.equals("boardIllegal"))
+                throw new Exception("boardIllegal");
+            if(result.equals("dictionaryIllegal"))
+                throw new Exception("dictionaryIllegal");
+            if(result.equals("error"))
+                throw new Exception("connectionError");
+            if(result.contains("success")){
+                String[] s = result.split(",");
+                int points = Integer.parseInt(s[1]);
+                this.points+=points;
+            }
+        } else
+            throw new Exception("connectionError");
+    }
+    public boolean challenge(Word word) throws Exception {
+        if (serverConnection.isConnected()) {
+            writer.write("C,"+word.toString());
+            writer.flush();
+            String result = reader.readLine();
+            if(result.equals("true"))
+                return true;
+            else
+                return false;
+
+        } else
+            throw new Exception("connectionError");
     }
 }
--- src/test/java/com/bookscrabble/client/model/GuestTest.java
+++ src/test/java/com/bookscrabble/client/model/GuestTest.java
@@ -1,2 +1,8 @@
-package com.bookscrabble.client.model;public class GuestTest {
+package com.bookscrabble.client.model;
+public class GuestTest {
+    public static void main(String[] args) {
+        Guest g =new Guest();
+        Host h = Host.getHost();
+
+    }
 }
